aedb3fecfda9ecf3223ea877609a3d8cd8cee060
James West <jwwest@gmail.com>
2011-11-19 17:03:39 -0600

    changing root

--- npo_catalyst/config/environment.rb
+++ npo_catalyst/config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 NpoCatalyst::Application.initialize!
+
+config.action_controller.relative_url_root = '/npo_catalyst'
